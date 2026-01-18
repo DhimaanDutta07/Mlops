fbf30012604afe5bfb012faed2abd45bc70f50b9
DD <dhimaanmams.07@gmail.com>
2026-01-18 17:00:26 +0000

    files updated

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -7,11 +7,8 @@ import mlflow
 import mlflow.sklearn
 import dagshub
 
-dagshub.init(
-    repo_owner="DhimaanDutta07",
-    repo_name="sec",
-    mlflow=True
-)
+
+dagshub.init(repo_owner='DhimaanDutta07', repo_name='mlflow', mlflow=True)
 
 class ModelTrainer:
     def __init__(self, config: ModelConfig):
@@ -21,6 +18,7 @@ class ModelTrainer:
     def train(self, X_train, y_train):
         mlflow.set_experiment("Mlops project")
         mlflow.sklearn.autolog()
+        mlflow.set_tracking_uri()
         with mlflow.start_run():
             self.model.fit(X_train, y_train)
             FileUtils.save_object(self.config.model_path, self.model)
