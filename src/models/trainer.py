d995e48694d7377a0363a508cfef334429bef434
DD <dhimaanmams.07@gmail.com>
2026-01-18 16:48:47 +0000

    files updated

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -1,6 +1,17 @@
+import mlflow.sklearn
+import mlflow.sklearn
 from sklearn.linear_model import LinearRegression
 from src.utils.common import FileUtils
 from src.config.config import ModelConfig
+import mlflow
+import mlflow.sklearn
+import dagshub
+
+dagshub.init(
+    repo_owner="DhimaanDutta07",
+    repo_name="sec",
+    mlflow=True
+)
 
 class ModelTrainer:
     def __init__(self, config: ModelConfig):
@@ -8,6 +19,9 @@ class ModelTrainer:
         self.model = LinearRegression()
 
     def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
-        FileUtils.save_object(self.config.model_path, self.model)
-        return self.model
+        mlflow.set_experiment("Mlops project")
+        mlflow.sklearn.autolog()
+        with mlflow.start_run():
+            self.model.fit(X_train, y_train)
+            FileUtils.save_object(self.config.model_path, self.model)
+            return self.model
